cb2788a6c9f340a2f820db74a869c213e57e73fe
Emiliano Loza <emilianolozac@gmail.com>
2023-04-05 00:52:45 -0600

    Se añadio botones para agregar y eliminar imagenes ademas de la logica para manipular el DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const minimum = 1;
+const maxium = 121; //valor sacado de la documentación de la API
 
-console.log('Happy hacking :)')
+const random = () => Math.floor(Math.random() * (maxium - minimum)) + minimum;
+
+const createImageNode = () =>{
+    const container = document.createElement('div');
+    container.className = 'p-4'
+
+    const image = document.createElement('img');
+    image.className = 'mx-auto';
+    image.width = '320';
+    image.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(image);
+
+    return container;
+};
+
+const mountNode = document.getElementById('images');
+const addButton = document.getElementById('btnAdd');
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.appendChild(newImage);
+};
+
+addButton.addEventListener('click', addImage);
+
+
